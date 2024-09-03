ed8e9838cff05830ed62811fdb4b009740879ad4
Mohammad AbuDayeh <Mohammad.Abudayeh@htu.edu.jo>
2024-09-03 01:26:15 +0000

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mohammad AbuDayeh",
+  title: "Software Engineer & Security checker",
+  email: "e@hotmail.com",
+  gitHub: "xDayeh",
+  instagram: "xDayeh",
+  linkedIn: "AbuDayeh",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "xDayeh",
+  youTube: "xDayeh",
 };
 
 const primaryColor = "#4E567E";
